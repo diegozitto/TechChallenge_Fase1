0209826d8969b6e1ff8b14e35ce01693c54df88d
Erick Costa <erick.silva@telefonicati.onmicrosoft.com>
2025-08-02 23:12:28 -0300

    Adiçao de Tag

--- src/main/java/com/restaurant/controller/ClienteController.java
+++ src/main/java/com/restaurant/controller/ClienteController.java
@@ -2,11 +2,15 @@ package com.restaurant.controller;
 
 import com.restaurant.entity.Cliente;
 import com.restaurant.repository.ClienteRepository;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clientes")
+@Tag(name = "Clientes", description = "Operações para Clientes")
 public class ClienteController extends AbstractUsuarioController<Cliente> {
 
     @Autowired
